500485b4571757273970e90a2212a727a20678de
shiva-uploder <ps8546572@gmail.com>
2025-05-03 22:42:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -157,7 +157,7 @@ async def remove_user(client, message: Message):
     except ValueError:
         await message.reply_text("Invalid command format. Use: /removeuser <user_id>")
 
-YOUR_ADMIN_ID = 7517045929
+YOUR_ADMIN_ID = 2088265361
 
 # Helper function to check admin privilege
 def is_admin(user_id):
